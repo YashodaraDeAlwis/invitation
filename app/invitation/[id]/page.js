1c6eaa6eae7943e66f4b9a9d2462aede5c1c94cd
Yashodara <yashdealwis@gmail.com>
2024-06-02 23:51:47 +0530

    update: update the content

--- app/invitation/[id]/page.js
+++ app/invitation/[id]/page.js
@@ -11,7 +11,7 @@ export async function generateMetadata({ params, searchParams }, parent) {
   const id = params.id;
   const inv = await fetchData(id);
 
-  const title = `Save the date | ${inv.data.invitor_name_for_card}`;
+  const title = `Save the Date | ${inv.data.invitor_name_for_card}`;
   const description = `Yashodara & Harshana Wedding Invitation  | ${inv.data.invitor_name_for_card}`;
 
   return {
